3823fede65ccf890221f35c17fc67193f0bdcddc
stevenliao <Stvnliao@yahoo.com>
2017-01-06 23:59:32 -0500

    making letter S

--- stocks/bokeh_letters.py
+++ stocks/bokeh_letters.py
@@ -1,15 +1,21 @@
 from bokeh.layouts import gridplot
 from bokeh.plotting import figure, output_file, show
 
-plot_options = {}
+# Options for each letter figure
+plot_options = {"width": 200, "height": 150}
 
-S = figure()
+# Axis
+x = list(range(11))
+y = [10, 10, 10, 10, 10, 10, 10, 10]
+
+S = figure(**plot_options)
+S.circle(x, y, color="navy", alpha=0.5)
 T = figure()
 E = figure()
 V = figure()
 N = figure()
 
 
-p = gridplot([[]])
+p = gridplot([[S]])
 output_file("letters.html")
 show(p)
